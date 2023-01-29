e51a030bf4c01e0003b36b896895acf9e7f0169c
Daniel Males <93955043+dasmorphy@users.noreply.github.com>
2023-01-29 15:03:00 -0500

    Update ventanilla.php

--- PRUEBA_PRACTICA/FRONT_BACK_END/ventanilla.php
+++ PRUEBA_PRACTICA/FRONT_BACK_END/ventanilla.php
@@ -1,8 +1,5 @@
 <?php
     require_once("./src/conexion.php");
-    
-    date_default_timezone_set('America/Guayaquil');
-    $fecha_actual = date("Y-m-d");
     $rand = rand();
 ?>
 
@@ -14,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tareas</title>
+    <title>Ventanilla</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
